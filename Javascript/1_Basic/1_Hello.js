3f8eaa0f8cb340f0061e9a840df3347de70329ac
Pruthviraj <93246061+Pruthviraj247@users.noreply.github.com>
2022-02-16 21:23:56 +0530

    Add files via upload

---
+++ Javascript/1_Basic/1_Hello.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Namaste World</title>
+</head>
+
+<body>
+    <script>
+        document.write("Namaste World!");
+        alert("Namaste World!");
+        console.log("Namaste World!");
+    </script>
+
+</body>
+
+</html>
